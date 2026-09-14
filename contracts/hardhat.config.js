841d4ef94794b7173e03b0a94c5e1613364af44b
usetech-nick <nkg9033@gmail.com>
2026-09-14 04:44:06 +0000

    Verify Escrow contract source on Etherscan

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -12,5 +13,8 @@ module.exports = {
       url: SEPOLIA_RPC_URL,
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
